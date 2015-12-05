34b7e569a5e0e1f075d3967e8f365d88ca763f0f
wass80 <wasss80@gmail.com>
2015-12-05 05:43:21 +0000

    show fab count

--- app/index.js
+++ app/index.js
@@ -2,18 +2,21 @@ import {getApi} from '../common/oauth';
 var setting = JSON.parse(localStorage["ulttwiclient"]);
 console.log(setting.tokens);
 
+function tiwdom(tweet){
+  var t = document.createElement("li");
+  t.innerText = tweet.text + "♥x" + tweet.favorite_count;
+  return t;
+}
 window.addEventListener('load',()=>{
   if(setting.tokens){
     var api = getApi(setting.tokens);
     var params = {screen_name: 'wass80'};
     var ul_tweet = document.getElementById("tweets");
-    api.get('statuses/user_timeline', params, function(error, tweets, response){
+    api.get('statuses/home_timeline', params, function(error, tweets, response){
       if (!error) {
         console.log('tweets',tweets.map((t)=>t.text).join("\n"), tweets);
         tweets.forEach((tweet)=>{
-            var t = document.createElement("li");
-            t.innerText = tweet.text;
-            ul_tweet.appendChild(t);
+            ul_tweet.appendChild(tiwdom(tweet));
         });
       } else {
         console.log('error', error.map((e)=>e.message).join("\n"),  error);
