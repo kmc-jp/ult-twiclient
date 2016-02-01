e55ce77498b98fb48d64216f2aeae6c012cab9f7
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-02-01 18:53:07 +0900

    Enable to run tests

--- init.js
+++ init.js
@@ -1,2 +1,14 @@
-require('electron-compile').init();
-require('./main')
+var path = require('path')
+var argv = require('yargs')
+  .default('test', false)
+  .argv
+
+if (argv.test) {
+  require('electron-compile').init()
+  var TestApplication = require('electron-jasmine').TestApplication
+  new TestApplication({specDirectory: 'spec'})
+}
+else {
+  require('electron-compile').init()
+  require('./main')
+}
