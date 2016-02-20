6b2d293c2fe921ce3b740b6c669874fee33f3bb5
wass80 <wasss80@gmail.com>
2016-02-20 22:55:01 +0000

    beautify common/oauth.js

--- common/oauth.js
+++ common/oauth.js
@@ -1,22 +1,22 @@
 import Oauth from 'node-twitter-api';
 import Twitter from 'twitter';
 
-const consumerKey = "STEtjjUGjw5bhdCjE1bdRxMh3",
-   consumerSecret = "B3mvDiqXsCp8xkoi4Fa8WlIKqzNA2mePSic5BSP0kBGW3SdmLj";
+const consumerKey = "STEtjjUGjw5bhdCjE1bdRxMh3";
+const consumerSecret = "B3mvDiqXsCp8xkoi4Fa8WlIKqzNA2mePSic5BSP0kBGW3SdmLj";
 
 const oauth = new Oauth({
-    consumerKey,
-    consumerSecret,
-    callback: 'http://twitter.com/'
+  consumerKey,
+  consumerSecret,
+  callback: 'http://twitter.com/'
 });
 
-var requestToken, requestTokenSecret;
+let requestToken, requestTokenSecret;
 export default {getOauthUrl, getTokens, getApi};
 function getOauthUrl(){
   return new Promise((resolve, reject)=>{
-    oauth.getRequestToken((error, _requestToken, _requestTokenSecret, results)=>{
+    oauth.getRequestToken((error, _requestToken, _requestTokenSecret)=>{
       if (error) {
-        console.error("Error getting OAuth request token : " + error);
+        console.error("Error getting OAuth request token",error);
         reject(error);
       } else {
         requestToken = _requestToken;
@@ -25,22 +25,22 @@ function getOauthUrl(){
       }
     });
   });
-};
+}
 function getTokens(oauth_verifier){
   return new Promise((resolve, reject)=>{
     oauth.getAccessToken(requestToken, requestTokenSecret, oauth_verifier,
-      (error, accessToken, accessTokenSecret, results)=>{
+      (error, accessToken, accessTokenSecret)=>{
         if (error) {
-          reject("error accessToken: "+error);
+          reject("error accessToken",error);
         } else {
           oauth.verifyCredentials(accessToken, accessTokenSecret,
-            (error, data, response)=>{
+            (error, data)=>{
               if (error) {
-                reject("error Credentials: "+error);
+                reject("error Credentials",error);
               } else {
                 console.log(accessToken, accessTokenSecret);
-                console.log("verified", data["screen_name"]);
-                var tokens = {
+                console.log("verified", data.screen_name);
+                const tokens = {
                   consumer_key: consumerKey,
                   consumer_secret: consumerSecret,
                   access_token_key: accessToken,
@@ -57,4 +57,4 @@ function getTokens(oauth_verifier){
 }
 function getApi(tokens){
   return new Twitter(tokens);
-};
+}
