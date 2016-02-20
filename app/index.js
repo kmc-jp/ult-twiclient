91ec8de6cdb0129efd3b5c222b16b390282fd82f
wass80 <wasss80@gmail.com>
2016-02-20 22:43:45 +0000

    beautify app/index.js

--- app/index.js
+++ app/index.js
@@ -1,21 +1,23 @@
 import {getApi} from '../common/oauth';
 const remote = require('electron').remote;
 const Menu = remote.Menu;
-const MenuItem = remote.MenuItem;
+//const MenuItem = remote.MenuItem;
 const clipboard = require('electron').clipboard;
 const dialog = remote.dialog;
 const path = require('path');
 const Vue = require('vue');
 require('twitter-text');
-var setting = JSON.parse(localStorage["ulttwiclient"] || '{}');
+const localStorageName = "ulttwiclient";
+const setting = JSON.parse(localStorage[localStorageName] || '{}');
 console.log(setting.tokens);
+let api = null;
 
 function anchorHTML(text, href) {
-  return '<a onClick="require(\'shell\').openExternal(\''+href+'\')" class="url">'+text+'</a>'
+  return `<a onClick="require('shell').openExternal('${href}')" class="url">'${text}'</a>`;
 }
 
 window.addEventListener('load',()=>{
-  Vue.filter('expand_url', function (text, entities){
+  Vue.filter('expand_url', (text, entities)=>{
     entities.urls.forEach((url)=>{
       text = text.replace(url.url, anchorHTML(url.display_url, url.expanded_url));
     });
@@ -26,10 +28,10 @@ window.addEventListener('load',()=>{
     }
     return text;
   });
-  Vue.filter('twemoji', function (text){
+  Vue.filter('twemoji', (text)=>{
     return twemoji.parse(text);
   });
-  Vue.filter('html_escape', function (text){
+  Vue.filter('html_escape', (text)=>{
     return text.replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
@@ -42,17 +44,17 @@ window.addEventListener('load',()=>{
       tweet: Object,
       replysender: Boolean
     },
-    data: function() {
-      return {vm}
+    data() {
+      return {vm};
     }
   });
-  Vue.filter('text_content', function (html){
-    let dom = document.createElement('div');
+  Vue.filter('text_content', (html)=>{
+    const dom = document.createElement('div');
     dom.innerHTML = html;
     return dom.firstChild.textContent;
   });
   // context menu on status
-  var contextMenuForTweet = Menu.buildFromTemplate([
+  const contextMenuForTweet = Menu.buildFromTemplate([
     {label: "ツイートに返信する", click: ()=>{vm.sendReply(vm.selectedTweet);}},
     {label: "ツイートをふぁぼる", click: ()=>{vm.favoriteTweet(vm.selectedTweet);}},
     {type: 'separator'},
@@ -61,10 +63,10 @@ window.addEventListener('load',()=>{
     {label: "ツイートのJSONを取得", click: ()=>{clipboard.writeText(JSON.stringify(vm.selectedTweet));}}
   ]);
   // context menu on image
-  var contextMenuForImage = Menu.buildFromTemplate([
+  const contextMenuForImage = Menu.buildFromTemplate([
     {label: "名前を付けて画像を保存", click: ()=>{vm.saveImage();}}
   ]);
-  var vm = new Vue({
+  const vm = new Vue({
     el: "#container",
     data: {
       newTweet: {
@@ -85,38 +87,39 @@ window.addEventListener('load',()=>{
       nowTime: new Date()
     },
     computed: {
-      calculateRemainChar: function() {
+      calculateRemainChar() {
         return this.maxTweetLength - twttr.txt.getTweetLength(this.newTweet.text);
       },
-      isLongTweet: function() {
+      isLongTweet() {
         return this.borderOfLongTweet > this.calculateRemainChar;
       },
-      isExceededTweet: function() {
+      isExceededTweet() {
         return this.calculateRemainChar < 0;
       }
     },
     methods: {
-      addTweet: function (tweet) {
-        let _tweet = this.body(tweet);
+      addTweet(tweet) {
+        const _tweet = this.body(tweet);
         if (_tweet.in_reply_to_status_id_str) {
           api.get('statuses/show', {id: _tweet.in_reply_to_status_id_str},
-            (error, in_reply_to_status, response)=>{
-            if (!error){
-              _tweet.in_reply_to_status = {};
-              Object.assign(_tweet.in_reply_to_status, in_reply_to_status);
-              this.insertToTweets(tweet);
-            } else {
-              console.error(error);
+            (error, in_reply_to_status)=>{
+              if (!error){
+                _tweet.in_reply_to_status = {};
+                Object.assign(_tweet.in_reply_to_status, in_reply_to_status);
+                this.insertToTweets(tweet);
+              } else {
+                console.error(error);
+              }
             }
-          });
+          );
         } else {
           this.insertToTweets(tweet);
         }
       },
-      insertToTweets: function (tweet) {
+      insertToTweets(tweet) {
         let i = 0;
         for(;i<this.tweets.length;i++){
-          if(this.tweets[i].id<tweet.id)break;
+          if(this.tweets[i].id<tweet.id){break;}
         }
         this.tweets.splice(i,0,tweet);
         const MAX_TWEETS = 300;
@@ -125,7 +128,7 @@ window.addEventListener('load',()=>{
         }
         this.modifyScroll();
       },
-      modifyScroll: function () {
+      modifyScroll() {
         const tweetsDOM = this.$els.tweets;
         const scrollTop = tweetsDOM.scrollTop;
         const topTweetDOM = tweetsDOM.firstElementChild;
@@ -135,7 +138,7 @@ window.addEventListener('load',()=>{
         }
         tweetsDOM.scrollTop += topTweetDOM.clientHeight;
       },
-      sendTweet: function (params) {
+      sendTweet(params) {
         api.post('statuses/update', {
           status: params.text,
           in_reply_to_status_id: params.in_reply_to_status_id
@@ -151,12 +154,12 @@ window.addEventListener('load',()=>{
           this.sendReplyDestTweet = {};
         });
       },
-      clearReply: function () {
+      clearReply() {
         this.newTweet.text = this.newTweet.text.replace(/@[a-zA-Z0-9_]+\s*/g, '');
         this.newTweet.in_reply_to_status_id = '';
         this.sendReplyDestTweet = {};
       },
-      startStreaming: function () {
+      startStreaming() {
         console.info("start streaming");
         this.streaming = true;
         api.stream('user', {}, (stream)=>{
@@ -185,73 +188,75 @@ window.addEventListener('load',()=>{
               // status
               this.addTweet(data);
               if (this.isMentionsForYou(data)) {
-                this.createNotification(data.user.name+" さんからあなた宛のメンションがあります。", data.text, data.user.profile_image_url_https, 'tweet');
+                this.createNotification(`${data.user.name} さんからあなた宛のメンションがあります。`, data.text, data.user.profile_image_url_https, 'tweet');
               }
             }
           });
           stream.on('favorite', (data)=>{
             if (data.target.screen_name === this.me.screen_name) {
-              this.createNotification("あなたのツイートが "+data.source.name+" にふぁぼられました。", data.target_object.text, data.target.profile_image_url_https, 'favorite');
+              this.createNotification(`あなたのツイートが ${data.source.name} にふぁぼられました。`, data.target_object.text, data.target.profile_image_url_https, 'favorite');
             }
           });
           stream.on('unfavorite', (data)=>{
             if (data.target.screen_name === this.me.screen_name) {
-              this.createNotification("あなたのツイートが "+data.source.name+" にふぁぼを取り消されました。", data.target_object.text, data.target.profile_image_url_https, 'unfavorite');
+              this.createNotification(`あなたのツイートが ${data.source.name} にふぁぼを取り消されました。`, data.target_object.text, data.target.profile_image_url_https, 'unfavorite');
             }
           });
           stream.on('follow', (data)=>{
             if (data.target.screen_name === this.me.screen_name) {
-              this.createNotification(data.source.name + " さんにフォローされました。", data.source.description, data.source.profile_image_url_https, 'follow');
+              this.createNotification(`${data.source.name} さんにフォローされました。`, data.source.description, data.source.profile_image_url_https, 'follow');
             }
           });
           stream.on('list_member_added', (data)=>{
             if (data.target.screen_name === this.me.screen_name) {
-              this.createNotification(data.source.name + " さんにリスト "+data.target_object.name+" に追加されました。", data.target_object.description, data.source.profile_image_url_https, 'list_member_added');
+              this.createNotification(`${data.source.name} さんにリスト ${data.target_object.name} に追加されました。`, data.target_object.description, data.source.profile_image_url_https, 'list_member_added');
             }
           });
         });
       },
-      selectTweet: function (tweet) {
+      selectTweet(tweet) {
         this.selectedTweet = tweet;
       },
-      toggleSelectedTweet: function (tweet) {
-        if (this.isSelectedTweet(tweet))
-          this.selectedTweet = {}
-        else
+      toggleSelectedTweet(tweet) {
+        if (this.isSelectedTweet(tweet)){
+          this.selectedTweet = {};
+        }else{
           this.selectTweet(tweet);
+        }
       },
-      isSelectedTweet: function(tweet) {
+      isSelectedTweet(tweet) {
         return this.selectedTweet && this.selectedTweet.id_str == tweet.id_str;
       },
-      favoriteTweet: function (tweet) {
+      favoriteTweet(tweet) {
         console.log(tweet.id_str);
-        var favorites_url = tweet.favorited ? 'favorites/destroy' : 'favorites/create';
-        var favorites_id = tweet.retweeted_status ? tweet.retweeted_status.id_str : tweet.id_str
+        const favorites_url = tweet.favorited ? 'favorites/destroy' : 'favorites/create';
+        const favorites_id = tweet.retweeted_status ? tweet.retweeted_status.id_str : tweet.id_str;
         api.post(favorites_url, {id: favorites_id}, (error, _tweet, response)=>{
           if (error) {
             this.createNotification("ツイートのふぁぼに失敗しました。", tweet.text, null, 'fail');
             return console.log('error', error.map((e)=>e.message).join("\n"),  error);
           }
           console.log(_tweet, response);
-          if (tweet.favorited)
+          if (tweet.favorited){
             this.createNotification("ツイートのふぁぼを取り消しました。", tweet.text, null, 'unfavorite');
-          else
+          }else{
             this.createNotification("ツイートをふぁぼりました。", tweet.text, null, 'favorite');
+          }
           Object.assign(tweet, _tweet);
         });
       },
-      showImage: function(url, size) {
+      showImage(url, size) {
         this.image = {url: url, width: size.w, height: size.h};
         this.showingMediaType = 'photo';
       },
-      showVideo: function(url, mimetype) {
+      showVideo(url, mimetype) {
         this.video = {url, mimetype};
         this.showingMediaType = 'video';
       },
-      showMedia: function(media) {
-        if(media.type == 'photo')
+      showMedia(media) {
+        if(media.type == 'photo'){
           this.showImage(media.media_url, media.sizes.small);
-        else {
+        }else{
           media.video_info.variants.some((v) => {
             if (v.content_type == 'video/mp4') {
               this.showVideo(v.url, v.content_type);
@@ -260,41 +265,42 @@ window.addEventListener('load',()=>{
           });
         }
       },
-      closeMedia: function() {
+      closeMedia() {
         this.image = {};
         this.video = {};
         this.showingMediaType = '';
       },
-      sendReply: function(tweet) {
+      sendReply(tweet) {
         console.log(tweet.id_str);
         this.sendReplyDestTweet = tweet;
-        this.newTweet.text = "@" + tweet.user.screen_name + " ";
+        this.newTweet.text = `@${tweet.user.screen_name} `;
         this.newTweet.in_reply_to_status_id = tweet.id_str;
         this.$els.submit_box.focus();
       },
-      hasMedias: function(tweet) {
+      hasMedias(tweet) {
         return 'extended_entities' in tweet && 'media' in tweet.extended_entities;
       },
-      isMentionsForYou: function(tweet) {
-        return tweet.text.match('@' + this.me.screen_name) && !tweet.retweeted_status;
+      isMentionsForYou(tweet) {
+        return tweet.text.match(`@${this.me.screen_name}`) && !tweet.retweeted_status;
       },
-      body: function (tweet){
+      body(tweet){
         return tweet.retweeted_status || tweet;
       },
-      createTwitterURL: function (tweet){
-        return 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
+      createTwitterURL(tweet){
+        return `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
       },
-      jumpToTwitterURL: function (tweet){
+      jumpToTwitterURL(tweet){
         require('shell').openExternal(this.createTwitterURL(tweet));
       },
-      deleteTweet: function(id_str) {
+      deleteTweet(id_str) {
         this.tweets.forEach((t,i) => {
-          if (t.id_str === id_str)
+          if (t.id_str === id_str){
             this.tweets.splice(i, 1);
-        })
+          }
+        });
       },
-      createNotification: function(title, body, icon, kind) {
-        let notification = {title, body, kind};
+      createNotification(title, body, icon, kind) {
+        const notification = {title, body, kind};
         this.notifications.push(notification);
         const removeMSecond = 6000;
         window.setTimeout(()=>{
@@ -304,20 +310,21 @@ window.addEventListener('load',()=>{
           new Notification(title, {icon: icon, body: body});
         }
       },
-      removeNotification: function(notification) {
+      removeNotification(notification) {
         this.notifications.forEach((n,i) => {
-          if (n === notification)
+          if (n === notification){
             this.notifications.splice(i, 1);
-        })
+          }
+        });
       },
-      contextMenuOnTweet: function(tweet) {
+      contextMenuOnTweet(tweet) {
         this.selectTweet(tweet);
         contextMenuForTweet.popup(remote.getCurrentWindow());
       },
-      contextMenuOnImage: function() {
+      contextMenuOnImage() {
         contextMenuForImage.popup(remote.getCurrentWindow());
       },
-      saveImage: function() {
+      saveImage() {
         const basename = path.basename(this.image.url);
         const defaultPath = path.dirname(require.main.filename) + basename;
         dialog.showSaveDialog({defaultPath}, (filename)=>{
@@ -326,37 +333,37 @@ window.addEventListener('load',()=>{
           });
         });
       },
-      detectKeyDown: function(event) {
+      detectKeyDown(event) {
         if (event.ctrlKey && event.keyIdentifier == "Enter" && document.activeElement == this.$els.submit_box) {
           this.sendTweet(this.newTweet);
         }
       },
-      convertTime: function(time) {
-        let date = new Date(time);
-        let diff = Math.floor((this.nowTime.getTime() - date.getTime()) / 1000);
+      convertTime(time) {
+        const date = new Date(time);
+        const diff = Math.floor((this.nowTime.getTime() - date.getTime()) / 1000);
         if (diff < 60) {
-          return diff + "秒";
+          return `${diff}秒`;
         } else if (diff < 3600) {
-          return Math.floor(diff / 60) + "分";
+          return `${Math.floor(diff / 60)}分`;
         } else if (diff < 86400) {
-          return Math.floor(diff / 3600) + "時間";
+          return `${Math.floor(diff / 3600)}時間`;
         } else if (diff < 31536000) {
-          return Math.floor(diff / 86400) + "日";
+          return `${Math.floor(diff / 86400)}日`;
         } else {
-          return Math.floor(diff / 31536000) + "年";
+          return `${Math.floor(diff / 31536000)}年`;
         }
       }
     }
   });
   if(setting.tokens){
-    var api = getApi(setting.tokens);
-    api.get('account/verify_credentials', {}, (error, data, response)=>{
+    api = getApi(setting.tokens);
+    api.get('account/verify_credentials', {}, (error, data)=>{
       if (error) {
         return console.log('error', error.map((e)=>e.message).join("\n"),  error);
       }
       vm.me = data;
     });
-    api.get('statuses/home_timeline', {count: 200}, function(error, tweets, response){
+    api.get('statuses/home_timeline', {count: 200}, (error, tweets)=>{
       if (error) {
         console.log('error', error.map((e)=>e.message).join("\n"),  error);
       }
